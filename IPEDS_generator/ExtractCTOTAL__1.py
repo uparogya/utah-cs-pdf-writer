140005a84538f2eb43c35485aba597112818e1b7
Arogya <109647530+uparogya@users.noreply.github.com>
2025-04-08 11:32:18 -0600

    arogya: c_total for individual institution and institution list

--- IPEDS_generator/ExtractCTOTAL__1.py
+++ IPEDS_generator/ExtractCTOTAL__1.py
@@ -1,41 +1,35 @@
 import pandas as pd
+import InstitutionList
 
 xls = pd.ExcelFile('./assets/IPEDS/IPEDS.xlsx')
 
 all_sheets = xls.sheet_names
-# sheets_data_frames = [pd.read_excel(xls, sheet_name=sheet, header=[0]) for sheet in all_sheets]
-
-excluded_unitid = []
+excluded_unitid = [433387]
 final_data = []
+unitid_totals = {}
 
 for sheet in all_sheets:
     data_frame = pd.read_excel(xls, sheet_name=sheet, header=[0])
-    ctotal = {}
-    
-    for i in range(len(data_frame)):
+    unitid_data = data_frame[~data_frame['UNITID'].isin(excluded_unitid)]
+    unitid_data = unitid_data.groupby(['UNITID', 'AWLEVEL'], as_index=False)[['CTOTALW', 'CTOTALM', 'CTOTALT']].sum()
+    unitid_totals[sheet] = unitid_data
 
+    ctotal = {}
+    for i in range(len(data_frame)):
         unitid = data_frame['UNITID'].iloc[i]
         if unitid in excluded_unitid:
             continue
-
         awlevel = data_frame['AWLEVEL'].iloc[i]
         ctotalw = data_frame['CTOTALW'].iloc[i]
         ctotalm = data_frame['CTOTALM'].iloc[i]
         ctotalt = data_frame['CTOTALT'].iloc[i]
-        
         if awlevel not in ctotal:
-            ctotal[awlevel] = {
-                'CTOTALW': 0,
-                'CTOTALM': 0,
-                'CTOTALT': 0
-            }
-        
+            ctotal[awlevel] = {'CTOTALW': 0, 'CTOTALM': 0, 'CTOTALT': 0}
         ctotal[awlevel]['CTOTALW'] += ctotalw
         ctotal[awlevel]['CTOTALM'] += ctotalm
         ctotal[awlevel]['CTOTALT'] += ctotalt
-    
-    year_data = []
 
+    year_data = []
     for awlevel, values in ctotal.items():
         year_data.append({
             'Year': sheet,
@@ -46,7 +40,7 @@ for sheet in all_sheets:
         })
     
     final_data.extend(year_data)
-    
+
     total_row = {
         'Year': sheet,
         'AWLEVEL': 'Total',
@@ -54,11 +48,47 @@ for sheet in all_sheets:
         'CTOTALM': sum(values['CTOTALM'] for values in ctotal.values()),
         'CTOTALT': sum(values['CTOTALT'] for values in ctotal.values())
     }
-    
     final_data.append(total_row)
 
-    
+base_columns = ['Year', 'AWLEVEL', 'CTOTALW', 'CTOTALM', 'CTOTALT']
+final_df = pd.DataFrame(final_data, columns=base_columns)
 
-final_df = pd.DataFrame(final_data)
-# final_df.sort_values(by=['Year', 'AWLEVEL'], ascending=[True, True], inplace=True)
-final_df.to_excel('./IPEDS_excel_extract/ctotal.xlsx', index=False)
+institution_columns = []
+institution_map = {}
+
+for sheet, unitid_data in unitid_totals.items():
+    for _, row in unitid_data.iterrows():
+        unitid = row['UNITID']
+        inst_name = InstitutionList.InstitutionList[unitid]["name"]
+        awlevel = row['AWLEVEL']
+        mask = (final_df['Year'] == sheet) & (final_df['AWLEVEL'] == awlevel)
+        row_idx = final_df.index[mask]
+        if row_idx.empty:
+            continue
+        for metric in ['CTOTALW', 'CTOTALM', 'CTOTALT']:
+            col_name = f'{inst_name}_{metric}'
+            if col_name not in final_df.columns:
+                final_df[col_name] = ''
+            final_df.loc[row_idx[0], col_name] = row[metric]
+            institution_map.setdefault(inst_name, []).append(col_name)
+
+institution_columns = []
+for inst_name in sorted(institution_map):
+    institution_columns.extend([
+        f'{inst_name}_CTOTALW',
+        f'{inst_name}_CTOTALM',
+        f'{inst_name}_CTOTALT'
+    ])
+final_df = final_df[base_columns + institution_columns]
+
+subheader_1 = base_columns.copy()
+subheader_2 = [''] * len(base_columns)
+
+for inst_name in sorted(institution_map):
+    subheader_1.extend([inst_name] * 3)
+    subheader_2.extend(['CTOTALW', 'CTOTALM', 'CTOTALT'])
+
+subheaders = pd.DataFrame([subheader_1, subheader_2], columns=final_df.columns)
+final_df_with_subheaders = pd.concat([subheaders, final_df], ignore_index=True)
+
+final_df_with_subheaders.to_excel('./IPEDS_excel_extract/ctotal.xlsx', index=False, header=False)
---
+++ IPEDS_generator/InstitutionList.py
@@ -0,0 +1,182 @@
+InstitutionList = {
+    230010:	{
+        "name": "Bridgerland Technical College"
+        },
+    230038:	{
+        "name": "Brigham Young University"
+        },
+    230065:	{
+        "name": "Cameo College of Essential Beauty"
+        },
+    230144:	{
+        "name": "Collectiv Academy"
+        },
+    230162:	{
+        "name": "Davis Technical College"
+        },
+    230171:	{
+        "name": "Utah Tech University"
+        },
+    230199:	{
+        "name": "Evans Hairstyling College-Cedar City"
+        },
+    230205:	{
+        "name": "Evans Hairstyling College-St George"
+        },
+    230214:	{
+        "name": "Paul Mitchell the School-Salt Lake City"
+        },
+    230366:	{
+        "name": "Eagle Gate College-Murray"
+        },
+    230418:	{
+        "name": "Ensign College"
+        },
+    230490:	{
+        "name": "Ogden-Weber Technical College"
+        },
+    230597:	{
+        "name": "Snow College"
+        },
+    230603:	{
+        "name": "Southern Utah University"
+        },
+    230676:	{
+        "name": "Uintah Basin Technical College"
+        },
+    230728:	{
+        "name": "Utah State University"
+        },
+    230737:	{
+        "name": "Utah Valley University"
+        },
+    230746:	{
+        "name": "Salt Lake Community College"
+        },
+    230764:	{
+        "name": "University of Utah"
+        },
+    230782:	{
+        "name": "Weber State University"
+        },
+    230807:	{
+        "name": "Westminster University"
+        },
+    380438:	{
+        "name": "Provo College"
+        },
+    406486:	{
+        "name": "Myotherapy Massage College"
+        },
+    421610:	{
+        "name": "Paul Mitchell the School-St. George"
+        },
+    433387:	{
+        "name": "Western Governors University"
+        },
+    441830:	{
+        "name": "Healing Mountain Massage School"
+        },
+    444787:	{
+        "name": "Nightingale College"
+        },
+    444857:	{
+        "name": "Skinworks School of Advanced Skincare"
+        },
+    445692:	{
+        "name": "Neumont College of Computer Science"
+        },
+    447263:	{
+        "name": "Joyce University of Nursing and Health Sciences"
+        },
+    447421:	{
+        "name": "Eagle Gate College-Layton"
+        },
+    448220:	{
+        "name": "Paul Mitchell the School-Logan"
+        },
+    448239:	{
+        "name": "Careers Unlimited"
+        },
+    448248:	{
+        "name": "Mountainland Technical College"
+        },
+    449816:	{
+        "name": "Southwest Technical College"
+        },
+    451574:	{
+        "name": "Aveda Institute-Provo"
+        },
+    451583:	{
+        "name": "Dixie Technical College"
+        },
+    455381:	{
+        "name": "Skin Science Institute"
+        },
+    456454:	{
+        "name": "Fortis College-Salt Lake City"
+        },
+    458104:	{
+        "name": "Renaissance Academie"
+        },
+    459541:	{
+        "name": "American Beauty Academy"
+        },
+    461272:	{
+        "name": "Mandalyn Academy"
+        },
+    461342:	{
+        "name": "Acaydia School of Aesthetics"
+        },
+    461494:	{
+        "name": "Taylor Andrews Academy of Hair Design-West Jordan"
+        },
+    461926:	{
+        "name": "Taylor Andrews Academy-St George"
+        },
+    474924:	{
+        "name": "Avalon Institute-Layton"
+        },
+    475495:	{
+        "name": "Rocky Mountain University of Health Professions"
+        },
+    475723:	{
+        "name": "Tooele Technical College"
+        },
+    476708:	{
+        "name": "The Barber School"
+        },
+    476993:	{
+        "name": "Medspa Academies"
+        },
+    480985:	{
+        "name": "Midwives College of Utah"
+        },
+    481100:	{
+        "name": "Top Nails & Hair Beauty School"
+        },
+    481456:	{
+        "name": "Bonnie Joseph Academy of Cosmetology & Barbering"
+        },
+    484394:	{
+        "name": "Taylor Andrews Academy of Hair Design-Provo"
+        },
+    489247:	{
+        "name": "American Beauty Academy-West Valley Campus"
+        },
+    494649:	{
+        "name": "Esteem Academy of Beauty"
+        },
+    495101:	{
+        "name": "Zion Massage College"
+        },
+    495411:	{
+        "name": "Paul Mitchell the School-Provo"
+        },
+    497268:	{
+        "name": "Arizona College of Nursing-Salt Lake City"
+        },
+    499307:	{
+        "name": "Galen Health Institutes-Salt Lake City"
+        },
+}
